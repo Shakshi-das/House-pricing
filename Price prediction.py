a6a640572798d9b5d0ad5b57787259d21c92db2a
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-06-17 23:46:39 +0530

    Update Price prediction.py
    Importing libraries for model training

--- Price prediction.py
+++ Price prediction.py
@@ -9,3 +9,14 @@ import random
 # Plots
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+# Models
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor, BaggingRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.linear_model import Ridge, RidgeCV
+from sklearn.linear_model import ElasticNet, ElasticNetCV
+from sklearn.svm import SVR
+from mlxtend.regressor import StackingCVRegressor
+import lightgbm as lgb
+from lightgbm import LGBMRegressor
+from xgboost import XGBRegressor
